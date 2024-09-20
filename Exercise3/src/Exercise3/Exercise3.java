ddc5d90f8303f90bfa05babe1e0019fa5eb72a48
mario.quintero <mario.quintero@106-25.info2>
2024-09-20 14:06:56 +0200

    ejercicio 3

---
+++ Exercise3/src/Exercise3/Exercise3.java
@@ -0,0 +1,11 @@
+package Exercise3;
+
+public class Exercise3 {
+	public static void main(String[] args) {
+		System.out.println("Mario Quintero Montero");
+		System.out.println();
+		System.out.println("Calle ancha n58");
+		System.out.println();
+		System.out.println("635921409");
+	}
+}
